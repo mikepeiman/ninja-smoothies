2b3c6589961679f235edba56a7e5373552e90566
Mike Peiman <mikepeiman@gmail.com>
2018-07-19 16:30:25 -0700

    setup new Vue Webpack project, integrated Materialize, created basic UI layout with placeholder component data

--- ninja-smoothies/src/components/Index.vue
+++ ninja-smoothies/src/components/Index.vue
@@ -1,33 +1,94 @@
 <template>
-  <div class="hello">
+  <div class="index container"> 
+      <div class="card blue-grey darken-1" v-for="smoothie in smoothies" :key="smoothie.id">
+        <div class="card-content white-text">
+          <h2 class="card-title">{{ smoothie.title }}</h2>
+          <ul class="ingredients">
+            <li v-for="(ingredient, index) in smoothie.ingredients" :key="index">
+              <span class="chip">{{ ingredient }}</span>
+            </li>
+          </ul>
+        </div>
+        <div class="card-action container">
+        <a href="" class="btn-floating large orange">
+          <router-link to="">
+            <i class="material-icons">edit</i>
+          </router-link>
+        </a>
+          <a href="" class="btn-floating large red">
+          <router-link to="">
+            <i class="material-icons">delete</i>
+          </router-link>
+        </a>
+        </div>
+  </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Index',
-  data () {
+  name: "Index",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      smoothies: [
+        {
+          title: "Ninja Brew",
+          slug: "ninja-brew",
+          ingredients: ["bananas", "coffee", "milk"],
+          id: 1
+        },
+        {
+          title: "Super Chill",
+          slug: "super-chill",
+          ingredients: ["bananas", "coffee", "milk"],
+          id: 2
+        },
+        {
+          title: "Morning Boost",
+          slug: "morning-boost",
+          ingredients: ["bananas", "coffee", "milk"],
+          id: 3
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.index {
+  display: flex;
+  flex-direction: row;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.card {
+    margin: 1em;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.card-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+} 
+.card-action {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
 }
-a {
-  color: #42b983;
+.card-title {
+  width: 100%;
+}
+.ingredients {
+  display: flex;
+}
+.card a.router-link-active,
+a.router-link-exact-active {
+  margin-right: 0 !important;
+}
+
+i.material-icons {
+  line-height: 1.7em;
+  margin: 0;
 }
 </style>
---
+++ ninja-smoothies/src/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="navbar">
+    <nav class="nav-extended indigo darken-2">
+      <div class="nav-content">
+        <div class="container">
+        <router-link to="">
+          <span class="nav-title">Ninja Smoothies</span>
+        </router-link>
+        </div>
+      <div class="container-column">
+        <a href="" class="btn-floating large blue">
+          <router-link to="">
+            <i class="material-icons">menu</i>
+          </router-link>
+        </a>        
+        <a href="" class="btn-floating large halfway-fab pulse pink">
+          <router-link to="">
+            <i class="material-icons">add</i>
+          </router-link>
+        </a>
+      </div>
+
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+.navbar nav {
+  padding: 0 20px;
+}
+nav .nav-content {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+}
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.container-column {
+  display: flex;
+  align-items: center;
+}
+nav i.material-icons {
+  line-height: 1.7em;
+}
+</style>
