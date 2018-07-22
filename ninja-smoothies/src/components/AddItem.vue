fe5c701e1024102a9fd764f02b7c118fa6417a80
Mike Peiman <mikepeiman@gmail.com>
2018-07-22 15:13:11 -0700

    added delete functionality per ingredient added; add and delete also function with Firestore DB

--- ninja-smoothies/src/components/AddItem.vue
+++ ninja-smoothies/src/components/AddItem.vue
@@ -5,7 +5,7 @@
     <h2 class="center-align indigo-text">
       Add New Item
     </h2>
-    <form @submit.prevent="AddItem">
+    <form @submit.prevent="addItem">
       <div class="field title">
         <label for="title">Smoothie Title</label>
         <input type="text" name="title" v-model="title">
@@ -14,7 +14,9 @@
         <label for="add-ingredient">Add An Ingredient</label>
         <input id="add-ingredient" type="text" name="add-ingredient" @keydown.tab.prevent="addIngredient" v-model="anotherIngredient">
         <ul v-if="ingredients">
-          <li v-for="ingredient in ingredients" class="chip">{{ ingredient }}</li>
+          <li v-for="(ingredient, index) in ingredients" :key="index" class="square-chip">{{ ingredient }}
+            <i class="material-icons delete" @click="deleteIngredient(ingredient)">delete</i>
+          </li>
         </ul>
       </div>
       <div class="field add-button center-align">
@@ -28,6 +30,9 @@
 </template>
 
 <script>
+import db from '@/firebase/init'
+import slugify from 'slugify'
+
 export default {
   name: 'AddSmoothie',
   data() {
@@ -35,17 +40,37 @@ export default {
       title: null,
       anotherIngredient: null,
       feedback: null,
-      ingredients: []
+      ingredients: [],
+      slug: null
     }
   },
   methods: {
-    AddItem() {
-      console.log(this.title)
+    addItem() {
+      // console.log(this.title, this.ingredients)
+      if(this.title) {
+        this.feedback = null
+        // create a slug
+        this.slug = slugify(this.title, {
+          replacement: '-',
+          remove: /[$*_=~.()'"!\-:@]/g,
+          lower: true
+        })
+        console.log(`Slug: ${this.slug}`)
+        db.collection('smoothies').add({
+          title: this.title,
+          ingredients: this.ingredients,
+          slug: this.slug
+        }).then(() => {
+          this.$router.push({ name: 'Index' })
+        })
+      } else {
+        this.feedback = 'Title is required'
+      }
     },
     addIngredient() {
       if(this.anotherIngredient) {
         this.ingredients.push(this.anotherIngredient)
-        console.log(this.ingredients)
+        // console.log(this.ingredients)
         const addField = document.querySelector('#add-ingredient')
         addField.value = ''
         this.anotherIngredient = null
@@ -55,6 +80,12 @@ export default {
         // error message to user, this field is required
         this.feedback = 'You must enter a value for ingredient'
       }
+    },
+    deleteIngredient(ingredient) {
+      console.log(ingredient)
+      this.ingredients = this.ingredients.filter(ing => {
+        return ing !== ingredient
+      })
     }
   }
 }
@@ -94,5 +125,17 @@ export default {
 
 .add-item .field {
   margin: 1.25em;
+}
+
+.square-chip {
+  display: inline-flex;
+  min-width: 50px;
+  background: rgba(50,75,100,0.25);
+  margin: 0 .5em;
+  padding: 5px 5px 5px 8px;
+  border-radius: 2px;
+}
+.square-chip i {
+  padding-left: .5em;
 }
 </style>
